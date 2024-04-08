ae9389324f14f6a00c99e0fb1e75f0d61f3b5428
Guintu <andreguintu26@gmail.com>
2024-04-08 13:16:22 +0800

    add

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.querySelector('a[href="#Main"]');
+    if(aboutLink) {
+        aboutLink.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            var aboutSection = document.getElementById('Main');
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.querySelector('a[href="#resume-section"]');
+    if(aboutLink) {
+        aboutLink.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            var aboutSection = document.getElementById('resume-section');
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.querySelector('a[href="#SkillsExperience"]');
+    if(aboutLink) {
+        aboutLink.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            var aboutSection = document.getElementById('SkillsExperience');
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.querySelector('a[href="#Contactss"]');
+    if(aboutLink) {
+        aboutLink.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            var aboutSection = document.getElementById('Contactss');
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
